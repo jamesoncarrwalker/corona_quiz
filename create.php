023ac2ff76881fc1b52e8ee654d3c642ec066621
jamesskywalker <jamescarr28@hotmail.co.uk>
2020-04-18 12:23:28 +0100

    clearer layout for creating a quiz

--- create.php
+++ create.php
@@ -9,27 +9,44 @@ include_once("bootstrap.php");
 
 $canCreateQuiz = false;
 $quizMasterId = null;
+$authDao = new AuthTokenDataAccessService(new PDOConn());
 if(isset($_POST['auth'])) {
-    $authDao = new AuthTokenDataAccessService(new PDOConn());
+
     if ($quizMasterId = $authDao->checkQuizMasterCredentials($_POST)) {
         $canCreateQuiz = true;
         $_SESSION['quizMasterId'] = $quizMasterId;
     } else {
         $_SESSION['error'] = 'Could not confirm your details';
+        $_SESSION['quizMasterId'] = null;
     }
-} else if (isset($_POST['createQuiz'])) {
-    $pdo = new PDOConn();
-    $quizDao = new QuizDataAccessService($pdo);
-    if($quizId = $quizDao->create($_POST)) {
-        $roundsDao = new RoundDataAccessService($pdo);
-        $roundsDao->addRounds($quizId,$_POST['round']);
-        header('location: manageQuiz.php?quiz_id=' . $quizId);
+}  else if (isset($_SESSION['quizMasterId'])) {
+    if ($quizMasterId = $authDao->checkQuizMasterToken($_SESSION['quizMasterId'])) {
+        $canCreateQuiz = true;
+        $_SESSION['quizMasterId'] = $quizMasterId;
     } else {
-        $_SESSION['error'] = 'Could not create quiz';
+        $_SESSION['error'] = 'Could not confirm your details';
+        $_SESSION['quizMasterId'] = null;
+    }
+}
+
+if (isset($_POST['createQuiz']) && $canCreateQuiz) {
+    $rounds = array_filter($_POST['round']);
+
+    if (count($rounds) > 0) {
+        $pdo = new PDOConn();
+        $quizDao = new QuizDataAccessService($pdo);
+        if ($quizId = $quizDao->create($_POST)) {
+            $roundsDao = new RoundDataAccessService($pdo);
+            $roundsDao->addRounds($quizId, $rounds);
+            header('location: manageQuiz.php?quiz_id=' . $quizId);
+        } else {
+            $_SESSION['error'] = 'Could not create quiz';
+        }
+    } else {
+        $_SESSION['error'] = 'You must have at least 1 round';
     }
 
 }
-
 
 ?>
 <!DOCTYPE html>
@@ -39,11 +56,7 @@ if(isset($_POST['auth'])) {
 
 <body>
 <?php include_once("template/header.php")?>
-<? if(isset($_SESSION['error'])){?>
-    <div class="container warning">
-        <p><?$_SESSION['error']?></p>
-    </div>
-<?}?>
+
 
 <div class="container">
     <div class="row">
@@ -56,41 +69,53 @@ if(isset($_POST['auth'])) {
         <script type="text/javascript">
             function addRound() {
 
-                $('.quiz_round').last().after('<input class="quiz_round" name="round[]" type="text">');
+                $('.quiz_round').last().after('<input class="quiz_round" name="round[]" type="text" placeholder="e.g. Sport,TV & Movies">');
 
             }
         </script>
         <div class="row">
             <form class="form-inline" action="create.php" method="post">
                 <div class="form-group mb-2">
-                    <label for="date" class="sr-only">date</label>
-                    <input name="date" type="number" class="form-control-plaintext" id="date" placeholder="123ABC">
+                    <input name="date" type="hidden" value="1" class="form-control-plaintext" id="date" placeholder="123ABC">
                 </div>
                 <div class="form-group mb-2">
-                    <label for="title" class="sr-only">Quiz name</label>
+                    <label for="title" >Quiz name</label>
+                    </br>
                     <input name="title" type="text" class="form-control-plaintext" id="title" placeholder="witty name">
                 </div>
-                <input class="quiz_round" name="round[]" type="text">
-                <button type="button" onclick="addRound()">Add Round</button>
-                <input type="hidden" value="<?echo $quizMasterId?>" name="host">
-                <button name="createQuiz" type="submit" class="btn btn-primary mb-2">Create Quiz</button>
+
+                </br>
+                </br>
+
+                <div class="form-group mb-2">
+                    <label for="round" >Rounds</label>
+                    </br>
+                    <input class="quiz_round" name="round[]" type="text" placeholder="e.g. Sport,TV & Movies">
+                     <button class="btn btn-success" type="button" onclick="addRound()">Add Round</button>
+                </div>
+                </br>
+                </br>
+                <div class="form-group mb-2">
+                    <input type="hidden" value="<?echo $quizMasterId?>" name="host">
+                    <button name="createQuiz" type="submit" class="btn btn-primary mb-2">Create Quiz</button>
+                </div>
             </form>
         </div>
 
 
     <? } else {?>
 
-    <div class="row">
+    <div class="row text-center">
         <form class="form-inline" action="create.php" method="post">
             <div class="form-group mb-2">
-                <label for="username" class="sr-only">Username</label>
+                <label for="username">Username</label>
                 <input name="username" type="text" class="form-control-plaintext" id="username" placeholder="username">
             </div>
             <div class="form-group mb-2">
-                <label for="password" class="sr-only">password</label>
+                <label for="password" >Password</label>
                 <input name="password" type="text" class="form-control-plaintext" id="password" placeholder="password">
             </div>
-            <button name="auth" type="submit" class="btn btn-primary mb-2">Join Quiz</button>
+            <button name="auth" type="submit" class="btn btn-primary mb-2">Make a Quiz</button>
         </form>
     </div>
     <?}?>
